d2b23876c2955617e1beb5ef3dd37b6025047741
tranm <tranm@NGOCMINH>
2025-03-04 21:23:32 +0700

    Add project files.

---
+++ MigrationWeb/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MigrationWeb.Models;
+
+namespace MigrationWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var product = _context.Products.ToList();
+            return View(product);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ MigrationWeb/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MigrationWeb.Models
+{
+   
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                : base(options) { }
+
+            public DbSet<Product> Products { get; set; }
+        }
+    
+}
